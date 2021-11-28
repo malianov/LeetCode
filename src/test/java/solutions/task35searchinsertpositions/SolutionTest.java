6327f255f9db1ad65082d0e61266d3a197afc786
Igor Malianov <imalianov@universe.dart.spb@imalianov.nb.ipa.dataart.net>
2021-11-28 18:13:34 +0200

    Task_35: Search Insert Positions

---
+++ src/test/java/solutions/task35searchinsertpositions/SolutionTest.java
@@ -0,0 +1,96 @@
+package solutions.task35searchinsertpositions;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Task_35 : Test Search Insert Positions")
+class SolutionTest {
+
+    @Nested
+    @DisplayName("Test method: isEmpty()")
+    class Task35 {
+
+        public Solution solution;
+
+        @BeforeEach
+        public void initEach() {
+            solution = new Solution();
+        }
+
+        @DisplayName("Test 1")
+        @Test
+        void test_1() {
+            // prepare
+            int[] nums = {1, 3, 5, 6};
+            int target = 5;
+
+            // when
+            int result = solution.searchInsert(nums, target);
+            int expected = 2;
+
+            // then
+            assertEquals(expected, result);
+        }
+
+        @DisplayName("Test 2")
+        @Test
+        void test_2() {
+            // prepare
+            int[] nums = {1, 3, 5, 6};
+            int target = 2;
+
+            // when
+            int result = solution.searchInsert(nums, target);
+            int expected = 1;
+
+            // then
+            assertEquals(expected, result);
+        }
+
+        @DisplayName("Test 3")
+        @Test
+        void test_3() {
+            // prepare
+            int[] nums = {1, 3, 5, 6};
+            int target = 7;
+
+            // when
+            int result = solution.searchInsert(nums, target);
+            int expected = 4;
+
+            // then
+            assertEquals(expected, result);
+        }
+
+        @DisplayName("Test 4")
+        @Test
+        void test_4() {
+            // prepare
+            int[] nums = {1, 3, 5, 6};
+            int target = 0;
+
+            // when
+            int result = solution.searchInsert(nums, target);
+            int expected = 0;
+
+            // then
+            assertEquals(expected, result);
+        }
+
+        @DisplayName("Test 5")
+        @Test
+        void test_5() {
+            // prepare
+            int[] nums = {1};
+            int target = 0;
+
+            // when
+            int result = solution.searchInsert(nums, target);
+            int expected = 0;
+
+            // then
+            assertEquals(expected, result);
+        }
+    }
+}
