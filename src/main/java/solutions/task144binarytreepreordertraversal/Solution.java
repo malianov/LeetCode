e7af68370139cea734b7b5697ff4738166b359b7
Igor Malianov <imalianov@universe.dart.spb@imalianov.nb.ipa.dataart.net>
2022-01-08 12:00:10 +0200

    Task 144 complete and tests are passed.

---
+++ src/main/java/solutions/task144binarytreepreordertraversal/Solution.java
@@ -0,0 +1,46 @@
+package solutions.task144binarytreepreordertraversal;
+
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+
+        if (root == null) {
+            return list;
+        }
+
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            list.add(node.val);
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return list;
+    }
+}
