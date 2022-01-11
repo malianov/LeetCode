eb0ab77e19039a18d20f784eddd4d10c6e3c4694
Igor Malianov <imalianov@universe.dart.spb@imalianov.nb.ipa.dataart.net>
2022-01-12 01:04:59 +0200

    Task 94 complete and tests are passed.

---
+++ src/main/java/solutions/task94binarytreeinordertraversal/task94binarytreeinordertraversal.java
@@ -0,0 +1,38 @@
+package solutions.task94binarytreeinordertraversal;
+
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode current = root;
+
+        while (!stack.isEmpty() || current != null) {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            } else {
+                current = stack.pop();
+                list.add(current.val);
+                current = current.right;
+            }
+        }
+        return list;
+    }
+}
