2b125d63c443f5a8813ce112be6f6b0754bb9403
Igor Malianov <imalianov@universe.dart.spb@imalianov.nb.ipa.dataart.net>
2022-01-16 10:31:42 +0200

    Task 107 complete and tests are passed.

---
+++ src/test/java/solutions/task107binarytreelevelordertraversalII/SolutionTest.java
@@ -0,0 +1,69 @@
+package solutions.task107binarytreelevelordertraversalII;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Task_107 : Binary Tree Level Order Traversal II")
+class SolutionTest {
+
+    @Nested
+    @DisplayName("Test method: levelOrder(TreeNode root)")
+    class Task3 {
+
+        public Solution solution;
+
+        @BeforeEach
+        public void initEach() {
+            solution = new Solution();
+        }
+
+        @DisplayName("Test 1")
+        @Test
+        void test_1() {
+            // prepare
+            TreeNode root = new TreeNode(3, new TreeNode(9, null, null), new TreeNode(20, new TreeNode(15, null, null), new TreeNode(7, null, null)));
+
+            // when
+            List<List<Integer>> result = solution.levelOrderBottom(root);
+            List<List<Integer>> expected = List.of(List.of(15,7), List.of(9,20), List.of(3));
+
+            // then
+            assertEquals(expected, result);
+        }
+
+        @DisplayName("Test 2")
+        @Test
+        void test_2() {
+            // prepare
+            TreeNode root = new TreeNode(1, null, null);
+
+            // when
+            List<List<Integer>> result = solution.levelOrderBottom(root);
+            List<List<Integer>> expected = List.of(List.of(1));
+
+            // then
+            assertEquals(expected, result);
+        }
+
+        @DisplayName("Test 3")
+        @Test
+        void test_3() {
+            // prepare
+            TreeNode root = null;
+
+            // when
+            List<List<Integer>> result = solution.levelOrderBottom(root);
+            List<List<Integer>> expected = new ArrayList<>();
+
+            // then
+            assertEquals(expected, result);
+        }
+    }
+}
