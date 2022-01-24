00548d0aa6cd0aba5c48403f22eb98c7aa10e942
Igor Malianov <imalianov@universe.dart.spb@imalianov.nb.ipa.dataart.net>
2022-01-24 02:17:00 +0200

    Solution with tests, works (streams)

--- src/main/java/solutions/task26removeduplicatesfromsortedarray/Solution.java
+++ src/main/java/solutions/task26removeduplicatesfromsortedarray/Solution.java
@@ -4,21 +4,7 @@ import java.util.*;
 
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        Map<Integer, Integer> map = new TreeMap<>();
-        for (int num : nums) {
-            if (map.containsKey(num)) {
-                map.put(num, map.get(num++));
-            } else {
-                map.put(num, 1);
-            }
-        }
-
-        int i = 0;
-        SortedSet<Integer> ss = new TreeSet<>(map.keySet());
-        for (Integer key : ss) {
-            nums[i++] = key;
-        }
-
-        return map.size();
+        nums = Arrays.stream(nums).sorted().distinct().toArray();
+        return nums.length;
     }
 }
