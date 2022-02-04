24411d15dfb96f4c8bacaa3c83420a10e56a85ab
Igor Malianov <imalianov@universe.dart.spb@imalianov.nb.ipa.dataart.net>
2022-02-04 23:34:01 +0200

    Solution without tests, works

---
+++ src/main/java/solutions/task28implementstrstr/Solution.java
@@ -0,0 +1,16 @@
+package solutions.task28implementstrstr;
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.equals("")) {
+            return 0;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i,needle.length() + i).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
